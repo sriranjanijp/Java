5b7489d318bc015f338f37d5b8ff04b6f3bcee09
sriranjanijp <sriranjani.jp@gmail.com>
2026-01-08 22:31:41 +0530

    Look Here - BTS

--- test.java
+++ test.java
@@ -1,29 +1,17 @@
-import java.util.*;
+import java.io.*;
 
-class Main
-{
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        int arr[] = {9,3,9,8,3,2};
-        int narr[] = Arrays.copyOfRange(arr,1,3);
+class MyException extends Exception {
+    public MyException(String message) {
+        super(message);
+    }
+}
 
-        for(int i =0;i<narr.length;i++)
-        {
-            System.out.print(narr[i]);
-        }
-        Arrays.sort(arr);
-        System.out.println();
-        for(int i =0;i<arr.length;i++)
-        {
-            System.out.print(arr[i]);
-        }
-        System.out.println();
-        for(int i =0, j = 0;i<10 && j<5;i++,j++)
-        {
-            System.out.println(i + " " + j );
-        }
-        char c = 'a';
-        System.out.println((char)65);
+class Main{
+    static void check() throws MyException, IOException{
+        throw new MyException("Custom Exception Occurred");
+    }
+
+    public static void main(String[] args) {
+       
     }
 }
